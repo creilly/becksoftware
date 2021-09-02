8989d5db05d43830a6f1725920045db29dcab69b
Chris Reilly <reilly.christopher@gmail.com>
2021-09-02 11:47:11 +0200

    converted auger motor to counter-based and made scans daq-card hardware time

--- libs/auger/motor.py
+++ libs/auger/motor.py
@@ -1,79 +1,60 @@
 from ctypes import *
-from .daqmx import *
+from daqmx import *
 from time import sleep
 
-def get_exponent(taskhandle):
-    strbuf = create_string_buffer(bufsize)
-    daqmx(
-        dll.DAQmxGetPhysicalChanName,
-        taskhandle,
-        None,
-        strbuf,
-        bufsize
-    )
-    return int(
-        strbuf.value.decode('utf8').split(', ')[0].split('/')[-1].split('line')[-1]
-    )
-
-def load_line(channel):
+def load_pulse_gen(channel):
     taskhandle = create_task()
-    add_global_channel(taskhandle,channel)
-    exp = get_exponent(taskhandle)
-    return (taskhandle,exp)
+    add_global_channel(taskhandle, channel)
+    set_samp_timing_type(taskhandle, IMPLICIT_TIMING)
+    return taskhandle
 
-def close_line(line):
-    taskhandle, exp = line
-    clear_task(taskhandle)
+def close_pulse_gen(handle):
+    clear_task(handle)
 
-def write_line(line,state):
-    handle, exp = line
-    daqmx(
-        dll.DAQmxWriteDigitalScalarU32,
-        handle,
-        True,
-        -1,
-        int(state) * 2 ** exp,
-        None
-    )
-
-def step_motor(line,delay,steps):
-    for step in range(steps):
-        write_line(line,False)
-        sleep(delay)
-        write_line(line,True)
-        sleep(delay)
+def step_motor(pulse_gen,steps):
+    set_samples(pulse_gen,steps)
+    start_task(pulse_gen)
+    task_wait(pulse_gen)
+    stop_task(pulse_gen)
         
-FORWARDS = False
-BACKWARDS = True
+FORWARDS = True
+BACKWARDS = False
 slope = 10000 / 0.20825 # steps per inch
-def move_sample(step_line,dir_line,displacement,delay):
+def move_sample(pulse_gen,dir_line,displacement):
+    steps = int(slope*abs(displacement))
+    if not steps:
+        return
     write_line(
         dir_line,
         FORWARDS if displacement > 0 else BACKWARDS        
     )
-    sleep(delay)
-    step_motor(step_line,delay,int(slope*abs(displacement)))
+    step_motor(pulse_gen,steps)
 
-def _move_sample(displacement,delay):
-    dirline = load_line('auger stepper motor direction')
-    stepline = load_line('auger stepper motor step')
-    move_sample(stepline,dirline,displacement,delay)
-    for line in (dirline,stepline): close_line(line)
+def _move_sample(displacement):
+    dirline = load_line('auger motor x dir')
+    pulsegen = load_pulse_gen('auger motor x clock')
+    move_sample(pulsegen,dirline,displacement)
+    close_line(dirline)
+    close_pulse_gen(pulsegen)
     
 if __name__ == '__main__':
-    dirline = load_line('auger stepper motor direction')
-    stepline = load_line('auger stepper motor step')
-    steps = 100
-    delay = 0.02
-    for direction in (True,False):
-        write_line(dirline,direction)
-        step = 0
-        while step < steps:        
-            step += 1
-            print(direction,step)
-            write_line(stepline,False)
-            sleep(delay)
-            write_line(stepline,True)
-            sleep(delay)
-    for line in (dirline,stepline):
-        close_line(line)
+    _move_sample(+0.050)
+    sleep(1.0)
+    _move_sample(-0.075)
+    _move_sample(+0.025)
+    # dirline = load_line('auger stepper motor direction')
+    # stepline = load_line('auger stepper motor step')
+    # steps = 100
+    # delay = 0.02
+    # for direction in (True,False):
+    #     write_line(dirline,direction)
+    #     step = 0
+    #     while step < steps:        
+    #         step += 1
+    #         print(direction,step)
+    #         write_line(stepline,False)
+    #         sleep(delay)
+    #         write_line(stepline,True)
+    #         sleep(delay)
+    # for line in (dirline,stepline):
+    #     close_line(line)
