4c45e4b088af9b2804ff1a74b63e780432fb1c3d
Chris Reilly <reilly.christopher@gmail.com>
2021-10-26 17:12:17 +0200

    added gentec power meter library. the thorlabs powermeter library still works but we are still having difficulty reliably communicated with the device

---
+++ libs/gentec.py
@@ -0,0 +1,41 @@
+import pyvisa
+
+VISA_ID = 'gentec'
+
+class GentecHandler:
+    def __init__(self,visa_id=VISA_ID):
+        self.pm = pm = open_pm(visa_id)
+        pm.baud_rate = 115200
+        pm.write_termination = '\r'
+        pm.read_termination = '\r'
+        
+    def __enter__(self):
+        return self.pm
+    def __exit__(self,*args):
+        close_pm(self.pm)
+
+def open_pm(visa_id=VISA_ID):
+    return pyvisa.ResourceManager().open_resource('COM4')
+
+def close_pm(pm):
+    pm.close()
+
+def send_command(pm,command):
+    return pm.query('*{}'.format(command))
+
+def get_version(pm):
+    return send_command(pm,'VER')
+
+def get_power(pm):
+    return float(send_command(pm,'CVU').strip().split(' ')[-1])
+
+if __name__ == '__main__':
+    from time import sleep
+    try:
+        print('gentec power monitor. press ctrl-c to quit.')
+        with GentecHandler() as pm:
+            while True:
+                print(get_power(pm))
+                sleep(0.200)
+    except KeyboardInterrupt:
+        print('interrupt received quitting...')
