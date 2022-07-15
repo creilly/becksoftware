48109ed1e778a552464dd5a882da4c4176d25739
Christopher Reilly <reilly.christopher@gmail.com>
2022-07-15 13:30:06 +0200

    fixed bugs in clientplot and forcing quit

--- programs/fitting/clientplot.py
+++ programs/fitting/clientplot.py
@@ -18,71 +18,69 @@ factors = args.factors
 
 client = communicator.Client(*communicator.get_pipes(args))
 
-config = communicator.get_config(args)
-
 datad = client.get_data(factors)
 
-client.quit()
+try:
+    config = communicator.get_config(args)
+    configmd = {s:dict(config.items(s)) for s in config.sections()}
 
-config = communicator.get_config()
-
-configmd = {s:dict(config.items(s)) for s in config.sections()}
-
-md = {
-    'factors':{
-        key:value for key, value in zip(
-            ('power','gamma','sigmaomega','tau'),
-            factors
-        )
-    }
-}
-
-md.update(configmd)
-
-if not os.path.exists(imagefolder):
-    os.mkdirs(imagefolder)
-
-with open(os.path.join(imagefolder,mdfname),'w') as f:
-    json.dump(md,f,indent=2)
-
-datafolder = communicator.get_data_folder()
-
-for lineindex, lined in datad.items():
-    for mode, computeds in lined.items():
-        deltaomegas, powers, measureds = sanitize.load_data(lineindex,mode,datafolder).tranpose()
-        xs, yms, ycs = zip(
-            *sorted(
-                zip(
-                    {sanitize.FC:powers,sanitize.FS:deltaomegas/(2.*np.pi)}[mode],
-                    measureds,
-                    computeds
-                )
+    md = {
+        'factors':{
+            key:value for key, value in zip(
+                ('power','gamma','sigmaomega','tau'),
+                factors
             )
-        )          
-        plt.plot(xs,yms,'.')
-        plt.plot(xs,ycs)
-        plt.title(
-            'line {:d} {}'.format(
-                lineindex,{
-                    sanitize.FC:'fluence curve',
-                    sanitize.FS:'frequency scan'
+        }
+    }
+
+    md.update(configmd)
+
+    if not os.path.exists(imagefolder):
+        os.makedirs(imagefolder)
+
+    with open(os.path.join(imagefolder,mdfname),'w') as f:
+        json.dump(md,f,indent=2)
+
+    datafolder = communicator.get_data_folder(args)
+
+    for lineindex, lined in datad.items():
+        for mode, computeds in lined.items():
+            deltaomegas, powers, measureds = sanitize.load_data(lineindex,mode,datafolder).transpose()
+            xs, yms, ycs = zip(
+                *sorted(
+                    zip(
+                        {sanitize.FC:powers,sanitize.FS:deltaomegas/(2.*np.pi)}[mode],
+                        measureds,
+                        computeds
+                    )
+                )
+            )          
+            plt.plot(xs,yms,'.')
+            plt.plot(xs,ycs)
+            plt.title(
+                'line {:d} {}'.format(
+                    lineindex,{
+                        sanitize.FC:'fluence curve',
+                        sanitize.FS:'frequency scan'
+                    }[mode]
+                )
+                
+            )
+            plt.xlabel(
+                {
+                    sanitize.FS:'frequency offset (megahertz)',
+                    sanitize.FC:'laser power (watts)'
                 }[mode]
             )
-            
-        )
-        plt.xlabel(
-            {
-                sanitize.FS:'frequency offset (megahertz)',
-                sanitize.FC:'laser power (watts)'
-            }[mode]
-        )
-        plt.ylabel('bolometer signal (normalized)') 
-        imagename = '{:03d}-{:d}.png'.format(lineindex,mode)     
-        print(imagename)
-        plt.savefig(
-            os.path.join(
-                imagefolder,
-                imagename
+            plt.ylabel('bolometer signal (normalized)') 
+            imagename = '{:03d}-{:d}.png'.format(lineindex,mode)     
+            print(imagename,flush=True)
+            plt.savefig(
+                os.path.join(
+                    imagefolder,
+                    imagename
+                )
             )
-        )
-        plt.cla()
+            plt.cla()
+finally:
+    client.quit()
