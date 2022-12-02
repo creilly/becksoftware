da11e6c14b0fda0f490bd0264e861281974559a9
Chris Reilly <reilly.christopher@gmail.com>
2022-12-02 17:59:13 +0100

    added configurability to hwpscan and measuring power using data stream

--- scripts/hwpscan.py
+++ scripts/hwpscan.py
@@ -2,51 +2,66 @@ import rotationstage as rot
 import gentec as pm
 from grapher import graphclient as gc
 import numpy as np
-import sys
+from time import time, sleep
 import topo
+import argparse
+
+ap = argparse.ArgumentParser(description='hwp scanning program')
+ap.add_argument('--name','-n',default='hwp scan',help='grapher dataset name')
+ap.add_argument('--time','-t',default=1.0,type=float,help='time per measurement')
+ap.add_argument('--settle','-l',default=1.0,type=float,help='settle time')
+ap.add_argument('--step','-s',type=float,default=2.0,help='step size (degrees)')
+ap.add_argument('--min','-m',default=10.0,type=float,help='starting angle')
+ap.add_argument('--max','-p',default=65.0,type=float,help='stopping angle')
+ap.add_argument(
+    '--folder','-f',default=gc.get_day_folder(),nargs='*',
+    help='grapher folder. enclose each subfolder in quotes and separate by whitespace.'
+)
+ap.add_argument('--info','-i',default='',help='dataset metadata note')
+
+
+args = ap.parse_args()
+name = args.name
+meastime = args.time
+anglestep = args.step
+anglemin = args.min
+anglemax = args.max
+folder = args.folder
+info = args.info
+settle = args.settle
 
 ic = topo.InstructionClient()
 
-if len(sys.argv) < 3:
-    folder = []
-else:
-    folder = [sys.argv[2]]
-if len(sys.argv) < 2:
-    name = 'hwp scan'
-else:
-    name = sys.argv[1]
-
-sn = None
-pmh = None 
-N = 1
-ao = 0.0
 with pm.GentecHandler() as pmh, rot.RotationStageHandler() as sn:
-    angles = np.arange(ao,ao+90.0,0.5)/1.0
+    angles = np.arange(anglemin,anglemax,anglestep)
 
     if not rot.is_homed(sn):
         print('stage not homed. homing...')
         rot.home(sn)
 
     path = gc.add_dataset(
-        gc.get_day_folder() + folder,
+        folder,
         name,
         (
             'hwp angle (degs)',
             'power (watts)',
             'ir photodiode (volts)'
-        )
+        ),
+        metadata = {'info':info} if info else {}
     )
 
     for angle in angles:
         rot.set_angle(sn,angle)
-        power = irpd = n = 0        
-        N = 10
-        while n < N:
-            n += 1
-            power += pm.get_power(pmh)
+        sleep(settle)
+        starttime = time()
+        pm.start_stream(pmh)
+        irpd = n = 0                
+        while True:
+            n += 1            
             irpd += ic.get_input(topo.FAST4)
-        power /= N
-        irpd /= N
+            if time() - starttime > meastime:
+                break                
+        irpd /= n
+        power = pm.stop_stream(pmh)
         print(angle,power,irpd)
         gc.add_data(path,(angle,power,irpd))
-    
